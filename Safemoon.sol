324e3ed0e8de5dd9cabadc62e2b1ee3a7b77772e
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-10-20 15:50:14 -0400

    Update Safemoon.sol

--- Safemoon.sol
+++ Safemoon.sol
@@ -755,7 +755,7 @@ contract SafeMoon is Context, IERC20, Ownable {
     constructor () public {
         _rOwned[_msgSender()] = _rTotal;
         
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F);
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02();
          // Create a uniswap pair for this new token
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
